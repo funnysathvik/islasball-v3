45b9f54ce287db3afe31c313f7e9e01c3e338892
funnysathvik <130309025+funnysathvik@users.noreply.github.com>
2024-01-26 08:21:31 -0700

    Add files via upload

---
+++ poop.js
@@ -0,0 +1,4 @@
+function work(){
+    document.getElementById("hllo").innerHTML = "dont cheat next time"
+    document.getElementById("pla").src = "rickastley.gif";
+}
---
+++ script.js
@@ -0,0 +1,27 @@
+score = 0;
+document.getElementById("poopski").innerHTML = "score - "+score;
+function shoot(){
+    shootvar = Math.random();
+    console.log(shootvar)
+    if (shootvar<0.5){
+        document.getElementById("play").src="islasmakes.png";
+        document.getElementById("hello").innerHTML="YOU MADE IT!!!!!!!"
+        score = score + 1;
+    }
+    else{
+        document.getElementById("play").src="islasmissbasketball.png";
+        document.getElementById("hello").innerHTML="you missed try again"
+    }
+    document.getElementById("shootbutton").onclick = rizz;
+}
+function reset(){
+    shootvar = 0;
+    document.getElementById("play").src="islasabouttoshootbasketball.png";
+    document.getElementById("hello").innerHTML="";
+    document.getElementById("shootbutton").onclick = shoot;
+}
+
+function rizz(){
+    document.getElementById("shootbutton").onclick = shoot;
+    window.location = "poop.html"
+}
