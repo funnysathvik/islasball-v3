1374704f3129037f528c6a178987900146c24f07
funnysathvik <130309025+funnysathvik@users.noreply.github.com>
2024-06-01 15:30:28 -0700

    Update poop.js

--- poop.js
+++ poop.js
@@ -2,3 +2,16 @@ function work(){
     document.getElementById("hllo").innerHTML = "dont cheat next time"
     document.getElementById("pla").src = "rickastley.gif";
 }
+
+function preload(){
+    music = loadSound("for the future.mp3");
+}
+
+function setup(){
+    
+}
+
+function draw(){
+   music.play();
+   music.loop();
+}
